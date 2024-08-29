ee54988b3ccf00b4205d3cc0d4a2c0018cf93096
Kedari <kedarisrinivasnaidu333@gmail.com>
2024-08-29 07:16:59 +0000

    first commit

---
+++ myapp/App.js
@@ -0,0 +1,10 @@
+var weather = require('./weather')
+var temperatureUnits = process.argv[2]
+var zipcodes = process.argv.slice(3)
+
+zipcodes.forEach(function (zipcode) {
+  weather.getWeather(zipcode, temperatureUnits)
+})
+
+// Idea:
+// Have user give 'current', '5-day', 'weekend', or some other string.
